658867a84c7953bd5ec558a24138bdd824c7db78
Dan Birman <danbirman@gmail.com>
2021-06-03 12:42:32 -0700

    Pushing final analysis script and closing out repo

--- analysis_DB.m
+++ analysis_DB.m
@@ -2,7 +2,7 @@
 
 % Load data
 [header,data] = csvreadh(fullfile('C:\proj\useful_vs_bias\useful_vs_bias.csv'));
-% data = data(1:5,:);
+data = data(1:9,:);
 
 header = header(1:16);
 % Split into the three datasets
@@ -14,6 +14,8 @@ bdata_pedig = data(:,33:48);
 udataz = (udata - repmat(nanmean(udata,2),1,size(udata,2))) ./ repmat(nanstd(udata,[],2),1,size(udata,2));
 bdata_backz = (bdata_back - repmat(nanmean(bdata_back,2),1,size(bdata_back,2))) ./ repmat(nanstd(bdata_back,[],2),1,size(udata,2));
 bdata_pedigz = (bdata_pedig - repmat(nanmean(bdata_pedig,2),1,size(bdata_pedig,2))) ./ repmat(nanstd(bdata_pedig,[],2),1,size(udata,2));
+
+dataz = [udataz bdata_backz bdata_pedigz];
 
 % Take the means
 udata_ = nanmean(udataz,1);
@@ -30,6 +32,10 @@ disp(sprintf('Correlation of background bias with pedigree bias: %1.2f',c(2,3)))
 
 axis_edge = ceil(max(abs(stacked_(:))));
 
+headerno_ = {};
+for hi = 1:length(header)
+    headerno_{hi} = strrep(header{hi},'_','');
+end
 % Make a basic plot
 h = figure;
 % Plot two plots, one with science vs background, the other plotting
@@ -41,6 +47,9 @@ ylabel('Bias related to background (s.d.)');
 axis equal
 axis([-1 1 -1 1]*axis_edge);
 title(sprintf('r = %1.2f',c(1,2)));
+for ti = 1:length(header)
+    text(stacked_(ti,1)+0.1,stacked_(ti,2)+0.1,headerno_{ti});
+end
 
 subplot(122);
 plot(stacked_(:,1),stacked_(:,3),'o','MarkerFaceColor','k','MarkerEdgeColor','w','MarkerSize',10);
@@ -49,6 +58,26 @@ ylabel('Bias related to pedigree (s.d.)');
 axis equal
 axis([-1 1 -1 1]*axis_edge);
 title(sprintf('r = %1.2f',c(1,3)));
+for ti = 1:length(header)
+    text(stacked_(ti,1)+0.1,stacked_(ti,3)+0.1,headerno_{ti});
+end
+
+%%
+r = corrcoef(dataz');
+figure; 
+imagesc(r);
+colormap gray;
+rup = triu(r);
+rup = rup(rup>0 & rup~=1);
+mean(rup)
+
+%% Standard order
+% standard_order = [
+% yale_order = [2 3 9 10 14 
+
+%% PCA space
+[coeff, score, latent, tsquared, explained] = pca(dataz);
+
 
 %% Some ideas to consider:
 % - is our data consistent across the lab?
